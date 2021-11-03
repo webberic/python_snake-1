a46d8a28b6db1c2e639556b8f7cc5eda9a02f4ef
Isaiah Webber <isaiahwebber@Isaiahs-MacBook-Pro.local>
2021-11-03 13:49:42 -0400

    add snake head motion

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ import pygame
 # initialize the game
 pygame.init()
 
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
 # set the screen size
-dis = pygame.display.set_mode((400, 300))
+dis = pygame.display.set_mode((800, 600))
 
 # draw the screen
 pygame.display.update()
@@ -21,10 +25,44 @@ pygame.display.set_caption('Snake game by Edureka')
 # variables to hold different colors
 blue   = (   0,   0,   255)
 red    = (   255,   0,   0)
-purple = (255,    0,   255)
 
 # create a variable to keep track of whether or not the game is over
 game_over = False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0
+y1_change = 0
+
+clock = pygame.time.Clock()
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis,blue,[x1,y1,10,10])
+
+    pygame.display.update()
+
+    clock.tick(30)
 
 # the main game loop. this loop will run infinitely until the value
 # of `game_over` changes from `False` to `True`
@@ -36,6 +74,8 @@ while not game_over:
             if event.type==pygame.QUIT:
                 game_over=True
                 #print(event)    #prints out all the actions that take place on the screen
+        pygame.draw.rect(dis,blue,[200,150,10,10])
+        pygame.display.update()
 
 
 # quit the game
